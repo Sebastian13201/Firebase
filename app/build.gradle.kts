5980c63acc5f0d89b77f14572feffd4656b80077
Sebastian13201 <sebastiangigi695@gmail.com>
2024-12-16 13:10:56 -0500

    Few more tweaks with authentication check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.google.firebase.crashlytics)
+    id("com.google.devtools.ksp")
 }
 
 android {
